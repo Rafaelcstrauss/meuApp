c88a05e652c16377f4f6711328c72ce8d976484b
Rafaelcstrauss <rafael.strauss@alunos.sc.senac.br>
2026-03-11 14:40:27 -0300

    TErminei

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,14 +2,14 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>teste</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large">teste</ion-title>
         </ion-toolbar>
       </ion-header>
 
@@ -17,12 +17,30 @@
         <strong>Ready to create an app?</strong>
         <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
       </div>
+
+      <IonInput v-model="nome"></IonInput>
+        <div>O nome é {{ nome }}</div>
+      
+        <IonButton @click="DirecionarSite">Tarefas</IonButton>
+
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonButton } from '@ionic/vue';
+import { ref } from 'vue';
+
+const nome = ref("");
+
+import router from '@/router';
+
+
+function DirecionarSite(){
+  router.push('/Tarefas')
+}
+
+
 </script>
 
 <style scoped>
---
+++ src/views/TarefasPage.vue
@@ -0,0 +1,66 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>TESTE</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        
+        <ion-content :fullscreen="true">
+            <ion-header collapse="condense">
+                <ion-toolbar>
+                    <ion-title size="large">oloko</ion-title>
+                </ion-toolbar>
+            </ion-header>
+
+            <ion-input v-model="novaTarefa">Digite sua tarefa</ion-input>
+            <div>tarefa nova: {{ novaTarefa }} </div>
+            <ion-button @click="Adicionar">Adicionar</ion-button>
+
+            <li v-for="(t, index) in (tare)" :key="index">
+                {{ t }} <ion-button @click="Remover(index)">Remover</ion-button>
+            </li>
+            
+            <div v-if="quantidade === 0 ">Sem bagulho cadastrado</div>
+
+            <ion-button @click="DirecionarSite">Voltar</ion-button>
+        </ion-content>
+    </ion-page>
+
+
+
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonToolbar, IonHeader, IonTitle, IonContent, IonInput, IonButton } from '@ionic/vue';
+import { ref } from 'vue';
+
+const tare = ref<string[]>( [] )
+const novaTarefa = ref('')
+const quantidade = ref(0);
+
+const Adicionar = () => {
+    if(novaTarefa.value.trim() === ""){
+        return
+    }
+    tare.value.push(novaTarefa.value);
+    quantidade.value++;
+}
+
+const Remover = (index:number) =>{
+    tare.value.splice(index,1)
+    quantidade.value--;
+}
+
+import router from '@/router';
+
+
+function DirecionarSite(){
+  router.push('/Home')
+}
+</script>
+
+
+<style scoped>
+
+</style>
